771853ed0c2b160006237bd61e54ff8af2408d5b
Caleb Cummins <caleb@caleb-desktop.lan>
2019-11-29 14:23:09 -0500

    dont edit efi vars install packages before install

--- grub/installgrub.sh
+++ grub/installgrub.sh
@@ -1,9 +1,9 @@
 DEV=$1
 echo "Installing to $DEV. Press enter to continue."
 read
+apt install grub-efi-amd64-signed grub-pc
 mkdir mnt
 sudo mount ${DEV}1 mnt
 sudo grub-install --boot-directory ./mnt/boot --target i386-pc $DEV
-sudo grub-install --boot-directory ./mnt/boot --target x86_64-efi --uefi-secure-boot --efi-directory ./mnt $DEV
-cp ./mnt/EFI/boot/grubx64.efi ./mnt/EFI/boot/bootx64.efi
+sudo grub-install --boot-directory ./mnt/boot --target x86_64-efi --uefi-secure-boot --efi-directory --no-nvram ./mnt $DEV
 sudo umount ./mnt
