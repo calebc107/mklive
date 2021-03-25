3ddea429e6cc02ec9f782f9a84f28e2fdfa005ac
null <null@null.com>
2021-03-25 14:53:26 -0400

    re-instate regular buster repository, specifically get firmware and kernel from backports

--- distros/buster/chroot.sh
+++ distros/buster/chroot.sh
@@ -2,7 +2,8 @@
 echo "#deb http://deb.debian.org/debian buster main contrib non-free
 deb http://deb.debian.org/debian buster-backports main contrib non-free" > /etc/apt/sources.list
 apt update
-apt install live-boot linux-image-amd64 sudo locales-all firmware-misc-nonfree
+apt install -t debian-backports linux-image-amd64 firmware-misc-nonfree
+apt install live-boot sudo locales-all
 dpkg-reconfigure locales-all
 update-initramfs.orig.initramfs-tools -ck $(ls /lib/modules | tail -n 1)
 passwd -d root
--- distros/buster/update.sh
+++ distros/buster/update.sh
@@ -10,7 +10,7 @@ read -p "Path is $path. Press enter if this is correct and script is running fro
 dhclient
 mv /usr/sbin/update-initramfs.* /usr/sbin/update-initramfs
 
-echo "#deb http://deb.debian.org/debian/ buster main contrib non-free
+echo "deb http://deb.debian.org/debian/ buster main contrib non-free
 deb http://deb.debian.org/debian/ buster-updates main contrib non-free
 deb http://security.debian.org/ buster/updates main contrib non-free
 deb http://deb.debian.org/debian buster-backports main contrib non-free
@@ -18,7 +18,7 @@ deb http://deb.debian.org/debian buster-backports main contrib non-free
 apt update
 apt dist-upgrade
 apt install gnome-core chromium nano bash-completion git htop squashfs-tools code net-tools 
-apt install -t buster-backports nvidia-driver
+apt install -t buster-backports nvidia-driver firmware-iwlwifi
 apt autoremove
 apt autoclean
 
