920ba195fab14a38314d08437038b4a6aa691589
caleb <none>
2021-07-19 17:46:44 -0400

    fix repo branch name add network-manager

--- distros/buster/chroot.sh
+++ distros/buster/chroot.sh
@@ -4,7 +4,7 @@ deb http://deb.debian.org/debian buster main contrib non-free
 deb http://deb.debian.org/debian buster-backports main contrib non-free
 " > /etc/apt/sources.list
 apt update
-apt install -t debian-backports linux-image-amd64 firmware-misc-nonfree firmware-iwlwifi
+apt install -t buster-backports linux-image-amd64 firmware-misc-nonfree firmware-iwlwifi network-manager
 apt install live-boot sudo locales-all
 dpkg-reconfigure locales-all
 update-initramfs.orig.initramfs-tools -ck $(ls /lib/modules | tail -n 1)
