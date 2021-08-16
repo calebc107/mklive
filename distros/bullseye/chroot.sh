e44d9089a4ad4a160640e8127954fcf262c11df1
caleb <none>
2021-08-15 22:38:51 -0400

    add support for debian 11

---
+++ distros/bullseye/chroot.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+set -e
+echo "
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+deb-src http://deb.debian.org/debian bullseye-backports main contrib non-free
+" > /etc/apt/sources.list
+apt update
+#TODO: re-instate backports as soon as packages are available for it
+#apt install -t bullseye-backports linux-image-amd64 network-manager firmware-iwlwifi firmware-brcm80211 firmware-atheros firmware-ralink firmware-realtek
+apt install linux-image-amd64 network-manager firmware-iwlwifi firmware-brcm80211 firmware-atheros firmware-ralink firmware-realtek
+apt install linux-image-amd64 network-manager firmware-iwlwifi firmware-brcm80211 firmware-atheros firmware-ralink firmware-realtek
+apt install live-boot sudo locales-all
+read -p "Set new hostname: " newhostname
+echo $newhostname > /etc/hostname
+cat /etc/hostname
+cat << END > /etc/hosts
+127.0.0.1	localhost
+127.0.1.1	$newhostname
+::1     localhost ip6-localhost ip6-loopback
+ff02::1 ip6-allnodes
+ff02::2 ip6-allrouters
+END
+dpkg-reconfigure locales-all
+update-initramfs.orig.initramfs-tools -ck $(ls /lib/modules | tail -n 1)
+passwd -d root
---
+++ distros/bullseye/update.sh
@@ -0,0 +1,103 @@
+#!/bin/bash
+set -e
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+path=$1
+read -p "Path is $path. Press enter if this is correct and script is running from tty2." continue
+
+mv /usr/sbin/update-initramfs.* /usr/sbin/update-initramfs || true
+
+echo "
+deb http://deb.debian.org/debian bullseye main contrib non-free
+deb http://deb.debian.org/debian bullseye-backports main contrib non-free
+deb http://deb.debian.org/debian/ bullseye-updates main contrib non-free
+deb http://security.debian.org/ bullseye-security main contrib non-free
+" > /etc/apt/sources.list
+apt update
+apt dist-upgrade
+apt install gparted lshw iperf3 avahi-daemon gnome-core chromium nano bash-completion git htop squashfs-tools net-tools curl wget
+#TODO: re-instate backports as soon as packages are available for it
+apt install nvidia-driver firmware-amd-graphics firmware-linux-* firmware-misc-nonfree
+#apt install -t bullseye-backports nvidia-driver firmware-amd-graphics firmware-linux-* firmware-misc-nonfree
+apt install nvidia-driver firmware-amd-graphics firmware-linux-* firmware-misc-nonfree
+apt autoremove
+apt autoclean
+
+timedatectl set-timezone America/New_York
+timedatectl set-local-rtc 1
+timedatectl set-ntp 1
+mount -o remount,rw /lib/live/mount/medium
+
+#allow user to switch to ramdisk if needed
+cat << END > /bin/live-toram
+#!/bin/bash
+echo \"Copying filesystem to memory.\"
+rsync -a --progress $path/filesystem.squashfs /tmp/live
+LANGUAGE=C LANG=C LC_ALL=C perl << EOF
+open LOOP, '</dev/loop0' or die \$!;
+open DEST, '</tmp/live' or die \$!;
+ioctl(LOOP, 0x4C06, fileno(DEST)) or die \$!
+close LOOP;
+close DEST;
+EOF
+umount /lib/live/mount/medium
+END
+
+read -p "Type username: " user
+adduser $user || true
+adduser $user sudo
+
+#set various gnome settings
+sudo -u $user dbus-launch dconf load /org/gnome/ << END
+[settings-daemon/plugins/media-keys]
+custom-keybindings=['/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+[settings-daemon/plugins/media-keys/custom-keybindings/custom0]
+binding='<Primary><Alt>t'
+command='gnome-terminal'
+name='Terminal'
+[settings-daemon/peripherals/keyboard]
+numlock-state='on'
+[settings-daemon/plugins/media-keys]
+custom-keybindings=['/settings-daemon/plugins/media-keys/custom-keybindings/custom0/', '/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
+[settings-daemon/plugins/media-keys/custom-keybindings/custom0]
+binding='<Primary><Alt>t'
+command='gnome-terminal'
+name='terminal'
+[shell]
+favorite-apps=['chromium.desktop', 'org.gnome.Nautilus.desktop', 'org.gnome.DiskUtility.desktop', 'org.gnome.Terminal.desktop']
+[nautilus/preferences]
+default-folder-viewer='icon-view'
+[nautilus/icon-view]
+default-zoom-level='standard'
+[desktop/interface]
+clock-show-seconds=true
+gtk-theme='Adwaita-dark'
+[desktop/peripherals/touchpad]
+tap-to-click=true
+END
+
+pkill -9 -u $user
+umount -l /home/$user || true
+rm -r /var/log/journal/* || true
+rm -r /var/apt/lists/* || true
+apt clean
+
+passwd -l root
+
+shopt -s dotglob
+mksquashfs / $path/newfilesystem.squashfs -noappend -wildcards -e 'dev/*' 'media/*' 'mnt/*' 'proc/*' 'lib/live/mount/*' 'usr/lib/live/mount/*' 'run/*' 'sys/*' 'tmp/*'
+#cp /boot/initrd.img* $path/newinitrd.img
+cp `ls /boot/initrd* | tail -n 1` $path/newinitrd.img
+#cp /boot/vmlinuz* $path/newvmlinuz
+cp `ls /boot/vmlinuz* | tail -n 1` $path/newvmlinuz
+
+read -p "Press enter to commit changes and reboot" continue
+mv $path/newfilesystem.squashfs $path/filesystem.squashfs
+mv $path/newinitrd.img $path/initrd.img
+mv $path/newvmlinuz $path/vmlinuz
+umount -l /
+echo "Unmounted all filesystems, including root. Rebooting..."
+reboot -f
